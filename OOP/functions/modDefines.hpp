9d4bfc0d850cf9fa67196f7107c59a86204df3b2
Vazar00 <aleksandr.mincenko01@gmail.com>
2026-05-20 16:10:19 +0300

    move VAR_PREFIX to EFL generic.hpp

--- OOP/functions/modDefines.hpp
+++ OOP/functions/modDefines.hpp
@@ -1,5 +1,4 @@
 
 #include "..\includes\classDefines.hpp"
 
-#define VAR_PREFIX (if (isNil "_ADDON_PREFX") then {SPREFX} else {_ADDON_PREFX})
 #define VAR_NAME (VAR_PREFIX + _name + (if (_instanceID < 0) then {""} else {"_" + str _instanceID}))
