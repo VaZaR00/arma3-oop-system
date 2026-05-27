0a5596d2da6020a2a941a8fc552528a5430f3978
Vazar00 <aleksandr.mincenko01@gmail.com>
2026-05-27 13:14:25 +0300

    add _ADDON_PREFX definition to modDefines.hpp

--- OOP/functions/modDefines.hpp
+++ OOP/functions/modDefines.hpp
@@ -2,3 +2,5 @@
 #include "..\includes\classDefines.hpp"
 
 #define VAR_NAME (format["%1%2%3%4", VAR_PREFIX, _varsPrefix, _name, (if (_instanceID < 0) then {""} else {"_" + str _instanceID})])
+
+_ADDON_PREFX = SPREFX;
